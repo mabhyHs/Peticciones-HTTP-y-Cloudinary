6dd645ade82c93f36f0516b0deb6ba831171ff76
mabhyHs <m4huens@gmail.com>
2022-10-05 17:24:28 -0300

    feature(upload cloudinary)

--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -1,72 +1,52 @@
-const jokeUrl     = 'https://api.chucknorris.io/jokes/random';
-const urlUsuarios = 'https://reqres.in/api/users?page=2';
+const jokeUrl = "https://api.chucknorris.io/jokes/random";
+const urlUsuarios = "https://reqres.in/api/users?page=2";
 
 // Cloudinary
-const cloudPreset = 'autwc6pa';
-const cloudUrl    = 'https://api.cloudinary.com/v1_1/dx0pryfzn/upload';
+const cloudPreset = "p6blnttv";
+const cloudUrl = "https://api.cloudinary.com/v1_1/dz4ejl4ym/upload";
 
-const obtenerChiste = async() => {
+const obtenerChiste = async () => {
+  try {
+    const resp = await fetch(jokeUrl);
 
-    try {
+    if (!resp.ok) throw "No se pudo realizar la peticion";
 
-        const resp = await fetch( jokeUrl );
+    const { icon_url, id, value } = await resp.json();
 
-        if( !resp.ok ) throw 'No se pudo realizar la peticion'; 
+    return { icon_url, id, value };
+  } catch (err) {
+    throw err;
+  }
+};
 
-        const { icon_url, id, value } = await resp.json();
+const obtenerUsuarios = async () => {
+  const resp = await fetch(urlUsuarios);
+  const { data: usuarios } = await resp.json();
 
-        return { icon_url, id, value };
-
-    } catch (err ){
-
-        throw err;
-
-    }
-    
-    
-}
-
-const obtenerUsuarios = async() => {
-
-    const resp = await fetch( urlUsuarios );
-    const { data:usuarios } = await resp.json();
-
-    return usuarios; 
-}
-
+  return usuarios;
+};
 
 // ArchivoSubir :: File
-const subirImagen = async( archivoSubir ) => {
+const subirImagen = async (archivoSubir) => {
+  const formData = new FormData();
+  formData.append("upload_preset", cloudPreset);
+  formData.append("file", archivoSubir);
 
-    const formData = new FormData();
-    formData.append('upload_preset', cloudPreset );
-    formData.append('file', archivoSubir );
+  try {
+    const resp = await fetch(cloudUrl, {
+      method: "POST",
+      body: formData,
+    });
 
-    try { 
-        
-        const resp = await fetch( cloudUrl, {
-            method: 'POST',
-            body: formData
-        });
-
-        if ( resp.ok ) {
-            const cloudResp = await resp.json();
-            return cloudResp.secure_url; 
-        } else {
-            throw await resp.json();
-        }
-
-    } catch( err ) {
-        throw err;
+    if (resp.ok) {
+      const cloudResp = await resp.json();
+      return cloudResp.secure_url;
+    } else {
+      throw await resp.json();
     }
+  } catch (err) {
+    throw err;
+  }
+};
 
-}
-
-
-export {
-    obtenerChiste,
-    obtenerUsuarios,
-    subirImagen
-}
-
-
+export { obtenerChiste, obtenerUsuarios, subirImagen };
